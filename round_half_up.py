1a6715415a753a230e332227f2beae70719e2897
riddhiwho <sharmariddhi81@gmail.com>
2023-04-19 23:45:03 +0530

    changed return value of -ve number to return float instead of Decimal

--- round_half_up.py
+++ round_half_up.py
@@ -3,8 +3,8 @@ from decimal import Decimal
 def round_half_up(num, d):
     if type(num)==float or type(num)==str:
         multiplier = 10 ** (d)
-        num=Decimal(num)
-        if num>0:  
+        num=Decimal(str(num))
+        if num>0:
             num = Decimal(str(num)) * multiplier
             new_num = Decimal(num) + Decimal('0.5')
             return int(new_num)/multiplier
@@ -12,6 +12,6 @@ def round_half_up(num, d):
             new_num=round_half_up(str(Decimal(num) *-1), d)*-1
             if new_num!=int(num*multiplier)/multiplier:
                 new_num=Decimal(str(new_num))+Decimal(str(2/multiplier))
-            return new_num        
+            return float(new_num)
     else:
         return float(num)
